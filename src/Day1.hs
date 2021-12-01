19a39b414c5d5d15d8e0b18e1d2d81223223a106
Thomas Berryhill <thomas@berryhill.me>
2021-12-01 00:30:03 -0500

    day1: use a nice windows function

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,26 +1,19 @@
 module Day1 (day1) where
 
 import Common
+import Data.List (tails)
 
 parser :: String -> [Int]
 parser = map read . lines
 
-pairs2 :: [a] -> [(a, a)]
-pairs2 [] = []
-pairs2 [_] = []
-pairs2 (x : y : rest) = (x, y) : pairs2 (y : rest)
-
-pairs3 :: [a] -> [(a, a, a)]
-pairs3 [] = []
-pairs3 [_] = []
-pairs3 [_, _] = []
-pairs3 (x : y : z : rest) = (x, y, z) : pairs3 (y : z : rest)
+windows :: Int -> [a] -> [[a]]
+windows n = filter ((== n) . length) . map (take n) . tails
 
 part1 :: [Int] -> Int
-part1 = length . filter (uncurry (<)) . pairs2
+part1 = length . filter (\[x, y] -> x < y) . windows 2
 
 part2 :: [Int] -> Int
-part2 = part1 . map (\(x, y, z) -> x + y + z) . pairs3
+part2 = part1 . map (\[x, y, z] -> x + y + z) . windows 3
 
 day1 :: Day
 day1 = Day 1 (simpleParser parser) part1 part2
