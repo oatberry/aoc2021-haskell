e95426fcfa1185aebbb370b734550d4f9de34382
Thomas Berryhill <thomas@berryhill.me>
2021-12-01 10:02:13 -0500

    day1: simply even more with trick...
    [a, b, c, d]
    a+b+c < b+c+d
     -b-c  -b-c
        a < d

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,19 +1,15 @@
 module Day1 (day1) where
 
 import Common
-import Data.List (tails)
 
 parser :: String -> [Int]
 parser = map read . lines
 
-windows :: Int -> [a] -> [[a]]
-windows n = filter ((== n) . length) . map (take n) . tails
-
-part1 :: [Int] -> Int
-part1 = length . filter (\[x, y] -> x < y) . windows 2
-
-part2 :: [Int] -> Int
-part2 = part1 . map (\[x, y, z] -> x + y + z) . windows 3
+checkSpans :: Ord a => Int -> [a] -> Int
+checkSpans n list =
+  length
+    . filter (uncurry (<))
+    $ zip list (drop n list)
 
 day1 :: Day
-day1 = Day 1 (simpleParser parser) part1 part2
+day1 = Day 1 (simpleParser parser) (checkSpans 1) (checkSpans 3)
