2bf909dbd7507d68a1fa160b94db0783d3aa640a
Thomas Berryhill <thomas@berryhill.me>
2021-12-06 20:35:01 -0500

    MONOIDS

--- src/Day4.hs
+++ src/Day4.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TupleSections #-}
 
 module Day4 (day4) where
@@ -11,6 +10,7 @@ import Control.Lens
 import Data.Distributive
 import Data.Function
 import Data.List (find)
+import Data.Monoid
 import qualified Data.Vector as Vec
 import Linear.V
 import Text.Megaparsec
@@ -22,7 +22,6 @@ type Row = (V 5 (Int, Bool))
 type Board = V 5 Row
 
 data Bingo = Bingo {_nums :: [Int], _boards :: [Board]}
-  deriving (Show)
 
 parser :: Parser Bingo
 parser = Bingo <$> (numsP <* "\n\n") <*> (boardP `sepBy` "\n\n")
@@ -31,8 +30,8 @@ parser = Bingo <$> (numsP <* "\n\n") <*> (boardP `sepBy` "\n\n")
     boardP = V <$> Vec.replicateM 5 (skipMany "\n" *> rowP) :: Parser Board
     rowP = V <$> Vec.replicateM 5 (space *> fmap (,False) decimal) :: Parser Row
 
-playBingo :: Bingo -> [(Int, [Board])]
-playBingo Bingo {_nums, _boards} = zip _nums . drop 1 $ scanl playRound _boards _nums
+bingoRounds :: Bingo -> [(Int, [Board])]
+bingoRounds Bingo {_nums, _boards} = zip _nums . drop 1 $ scanl playRound _boards _nums
   where
     playRound boards number =
       boards
@@ -48,20 +47,14 @@ boardScore (num, board) = num * sumOf unmarkedCells board
 isSolved :: Board -> Bool
 isSolved board = ((||) `on` any (all snd)) board (distribute board)
 
-findBoard :: ([Board] -> Maybe Board) -> [(Int, [Board])] -> Maybe (Int, Board)
-findBoard when = go
+playBingo ::
+  (Functor f, Monoid (f (Int, Board))) =>
+  (Maybe (Int, Board) -> f (Int, Board)) ->
+  Bingo ->
+  f Int
+playBingo monoid = fmap boardScore . mconcat . map findBoard . bingoRounds
   where
-    go [] = Nothing
-    go ((num, boards) : rest) = fmap (num,) (when boards) <|> go rest
-
-part1 :: Bingo -> Maybe Int
-part1 = fmap boardScore . findBoard (find isSolved) . playBingo
-
-part2 :: Bingo -> Maybe Int
-part2 = fmap boardScore . findBoard lastWinner . playBingo
-  where
-    lastWinner [a] = if isSolved a then Just a else Nothing
-    lastWinner _ = Nothing
+    findBoard (num, boards) = monoid . fmap (num,) . find isSolved $ boards
 
 day4 :: Day
-day4 = Day 4 parser part1 part2
+day4 = Day 4 parser (getFirst . playBingo First) (getLast . playBingo Last)
